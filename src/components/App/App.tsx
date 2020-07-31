9772ba1acbdeeef0b1539281b45f2a91eb9fe2a9
lediepts <dieplh.elite@gmail.com>
2020-08-01 01:10:14 +0900

    update settingfile

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import './app.scss';
+import React from "react";
+import "./app.scss";
 
 function App() {
-  const handleClick = ()=>{
-    console.log("object")
-  }
-  return (
-    <div className="App">
-      <header>
-        <a
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={()=>handleClick()} >Click Me</button>
-      </header>
-    </div>
-  );
+	const handleClick = () => {
+		console.log("object");
+	};
+	return (
+		<div>
+			<header>
+				<a href="#" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+				<button onClick={() => handleClick()}>Click Me</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
